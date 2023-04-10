d2c291ac02d63e96960de84cbea1e46550dc4dc3
shaunhwq <48981455+shaunhwq@users.noreply.github.com>
2023-04-10 19:56:41 +0800

    Temp fix for #2, slower frame seek but correct
    Due to the OpenCV bug, we are not able to retrieve the correct frame
    for comparing when using cap.set(). Alternative solution (slower)
    is to call cap.grab() until we reach the desired frame.
    
    However, this method is slower and seeking to a frame earlier than its
    current position will require that the video be reloaded. E.g.
    cv2.VideoCapture() again.

--- visual_comparison/managers/content_manager.py
+++ visual_comparison/managers/content_manager.py
@@ -85,14 +85,34 @@ class ContentManager:
         return len(self.video_indices) != 0
 
     def set_video_position(self, value):
+        """
+        # https://github.com/opencv/opencv/issues/9053
+        cap.grab() mentioned to be the solution for now.
+
+        Will be slow when want to adjust frames to position before current idx.
+        E.g. current = frame 50, desired = frame 10
+        """
+        content_paths = self.get_paths()
+
+        def seek_video(vid_idx, no_frames, in_future):
+            if not in_future:
+                self.content_loaders[vid_idx] = cv2.VideoCapture(content_paths[vid_idx])
+            for i in range(no_frames):
+                self.content_loaders[vid_idx].grab()
+
         if not self.has_video():
             return
 
         first_cap = self.content_loaders[self.video_indices[0]]
-        video_position = int(value / 100 * first_cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        current_position = int(first_cap.get(cv2.CAP_PROP_POS_FRAMES))
+        desired_position = int(value / 100 * first_cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+        is_in_future = current_position < desired_position
+        number_frames = desired_position - current_position if is_in_future else desired_position
+
+        # TODO: Might not be best solution, need to check if we can use threads to speed up this IO task
         for video_idx in self.video_indices:
-            cap = self.content_loaders[video_idx]
-            cap.set(cv2.CAP_PROP_POS_FRAMES, video_position)
+            seek_video(video_idx, number_frames, is_in_future)
 
     def get_video_position(self):
         if not self.has_video():
